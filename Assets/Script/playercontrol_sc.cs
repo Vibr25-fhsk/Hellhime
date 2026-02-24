3155fa62198de29c52160edcf8a573cf37643536
Vibr25-fhsk <sspel25vibr@axevalla.fhsk.se>
2026-02-24 15:36:01 +0100

    *_*

--- Assets/Script/playercontrol_sc.cs
+++ Assets/Script/playercontrol_sc.cs
@@ -1,29 +1,47 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 public class playercontorl_sc : MonoBehaviour
 {
+    public bool isFirstPlayer;
+
     #region movement
     [SerializeField]private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveinput;
-
-    
-
+    float horizontalMovement;
     #endregion
+
+    private Index_sc index_Sc;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); 
     }
+    void Awake()
+    {
 
+
+        
+    }
     // Update is called once per frame
     void Update()
     {
-       rb.linearVelocity = moveinput * moveSpeed; 
+        if(isFirstPlayer == true)
+        {
+            horizontalMovement = Input.GetAxisRaw("Horizontal");
+        }
+        else
+        {
+            horizontalMovement = Input.GetAxisRaw("Horizontal_P2");
+        }
+    
+       rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y); 
     }
-    public void Move(InputAction.CallbackContext context)
+    public void Move(InputAction.CallbackContext moveContext)
     {
-        moveinput = context.ReadValue<Vector2>();
+        moveinput = moveContext.ReadValue<Vector2>();
     }
 
 }
