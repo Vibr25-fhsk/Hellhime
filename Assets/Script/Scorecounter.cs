bc02f4af2596b58cb6e2aa2eb5e6d2d7121d3afd
Victor <v04.bruhn@gmail.com>
2026-05-19 17:47:01 +0200

    working

--- Assets/Script/Scorecounter.cs
+++ Assets/Script/Scorecounter.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+
 public class Scorecounter : MonoBehaviour
 {
     int wincounters;
@@ -7,23 +9,26 @@ public class Scorecounter : MonoBehaviour
     TMP_Text Player2wins;
     TMP_Text Player1wins;
     GameManeger GM;
+
+    public bool Scoreupdate = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
         Player2wins = GameObject.Find("P2Win_num").GetComponent<TMP_Text>();
         Player1wins = GameObject.Find("P1Win_num").GetComponent<TMP_Text>();
+        
         GM = GameObject.Find("Hella_GM").GetComponent<GameManeger>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+        //SceneManager.GetLastActiveScene().name =="Homescreen"
 
         Player1wins.text = GM.P1Wins.ToString(); 
         Player2wins.text = GM.P2Wins.ToString();
+    
         
     }
 }
