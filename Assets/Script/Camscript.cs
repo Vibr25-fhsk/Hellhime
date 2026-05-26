00033f8719eff286917540b5beae126e0677077b
Vibr25-fhsk <sspel25vibr@axevalla.fhsk.se>
2026-05-26 16:09:18 +0200

    0.0.4

--- Assets/Script/Camscript.cs
+++ Assets/Script/Camscript.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-
-public class Campscript : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class campscript : MonoBehaviour
 {
     public Camera cam;
-    float defoultScreenHight = 1080f;
+    float defoultScreenHight = 1440f;
     float defoultFOV = 53f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,5 +16,24 @@ public class Campscript : MonoBehaviour
     void OnScreenRezized()
     {
         cam.fieldOfView = defoultFOV * (Screen.height / defoultScreenHight);
+        if(SceneManager.GetActiveScene().name == "Menue")
+        {
+            defoultScreenHight = 1440f;
+            defoultFOV = 50f;
+            if(cam != null)
+            {
+                cam.fieldOfView = defoultFOV * (Screen.height / defoultScreenHight);
+            }
+        }
+
+        if(SceneManager.GetActiveScene().name=="Hellhime")
+        {
+            defoultScreenHight = 1440f;
+            defoultFOV = 50f;
+            if(cam != null)
+            {
+                cam.fieldOfView = defoultFOV * (Screen.height / defoultScreenHight);
+            }
+        }
     }
 }
